bf9138957babd1927e1578cced9b05456e1d310c
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-20 17:05:11 -0600

    Update App.tsx

--- apps/user-mobile-app/App.tsx
+++ apps/user-mobile-app/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { CartProvider } from "./src/context/CartContext"; // 🔥 NUEVO
+
 import Login from "./src/screens/LoginScreen";
 import Home from "./src/screens/HomeScreen";
 import Cart from "./src/screens/CartScreen";
@@ -11,14 +13,16 @@ const Stack = createNativeStackNavigator();
 
 export default function App(){
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login}/>
-        <Stack.Screen name="Home" component={Home}/>
-        <Stack.Screen name="Cart" component={Cart}/>
-        <Stack.Screen name="Checkout" component={Checkout}/>
-        <Stack.Screen name="Tracking" component={Tracking}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CartProvider> {/* 🔥 ENVUELVE TODO */}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login}/>
+          <Stack.Screen name="Home" component={Home}/>
+          <Stack.Screen name="Cart" component={Cart}/>
+          <Stack.Screen name="Checkout" component={Checkout}/>
+          <Stack.Screen name="Tracking" component={Tracking}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
 }
