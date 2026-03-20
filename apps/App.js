ec1aa5f31231d662f71a1df95ce6b7c4fd2d62e8
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-20 16:58:06 -0600

    Create App.js

---
+++ apps/App.js
@@ -0,0 +1,9 @@
+import { CartProvider } from './src/context/CartContext';
+
+export default function App() {
+  return (
+    <CartProvider>
+      {/* tu navegación */}
+    </CartProvider>
+  );
+}
