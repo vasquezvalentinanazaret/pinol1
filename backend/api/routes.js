c82f45bd1522cdc0382b3a03f1bbc5e0518d2f27
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-20 16:37:02 -0600

    Delete backend directory

--- backend/api/routes.js
+++
@@ -1,7 +0,0 @@
-
-const router = require('express').Router();
-
-router.use('/auth', require('../modules/auth/routes'));
-router.use('/orders', require('../modules/orders/routes'));
-
-module.exports = router;
